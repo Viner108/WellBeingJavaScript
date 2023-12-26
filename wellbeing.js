b59a9db3b225d23524a7841ce4b69a698662b941
Viner108 <andrukovichl@mail.ru>
2023-12-26 17:37:40 +0300

    list data commit

--- wellbeing.js
+++ wellbeing.js
@@ -3,49 +3,62 @@ function postRequest() {
     var headAche = document.getElementById("headAche");
     var pressure = document.getElementById("pressure");
     var date = new Date();
-    var  year = date.getFullYear();
-    var  month = date.getMonth()+1;
-    var  day = date.getDate();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
     var body = {
-        "headAche" : headAche.value,
-        "pressure" : pressure.value,
-        "date" : year+"-"+month+"-"+day,
-        "userId" : 2
+        "headAche": headAche.value,
+        "pressure": pressure.value,
+        "date": year + "-" + month + "-" + day,
+        "userId": 2
     }
     xhr.open("POST", "http://192.168.1.102:8080/android/postDTO", true);
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var response = JSON.parse(xhr.responseText);
         }
     }
     ;
-    if(headAche.value!="" & pressure.value!=""){
+    if (headAche.value != "" & pressure.value != "") {
         xhr.send(JSON.stringify(body));
-    }else{
+    } else {
         alert("Заполните все поля")
     }
 }
+
 function getList() {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://192.168.1.102:8080/android", true);
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText);
+    xhr.onreadystatechange = function () {
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState != 4) {
+                return;
+            }
+            if (xhr.status === 200) {
+                var result = JSON.parse(xhr.responseText);
+                var array = [];
+                for (var i = 0; i < result.length; i++) {
+                    var userHealth = new UserHealthDTO(result[i].headAche,result[i].pressure,result[i].userId,result[i].date);
+                    array.push(userHealth);
+                }
+                var html = array.map((item) => `<li>Головная боль: ${item.headAche}, давление: ${item.pressure}, id пользователя: ${item.userId}, дата: ${item.date} </li><br>`).join('');
+                document.querySelector('ul').innerHTML = html;
+            } else {
+                console.log('err', xhr.responseText);
+            }
         }
     };
     xhr.send();
 }
-// class UserHealthDTO {
-//   constructor(headAche,pressure) {
-//     this.headAche=headAche;
-//     this.pressure=pressure;
-//     this.userId=2;
-//     var date = new Date();
-//     var  year = date.getFullYear();
-//     var  month = date.getMonth()+1;
-//     var  day = date.getDate();
-//     this.date= year+"-"+month+"-"+day;
-//   }
-// }
+getList();
+
+class UserHealthDTO {
+  constructor(headAche,pressure,userId,date) {
+    this.headAche=headAche;
+    this.pressure=pressure;
+    this.userId=userId;
+    this.date=date;
+  }
+}
